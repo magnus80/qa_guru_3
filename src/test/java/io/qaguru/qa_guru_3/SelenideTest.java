c6d2529cbd16658ca0d2d8c57f5f4ace13701ae4
konstantin.iryshkov <zarechny@inbox.ru>
2021-02-16 13:36:06 +0300

    Fixes according to comment

--- src/test/java/io/qaguru/qa_guru_3/SelenideTest.java
+++ src/test/java/io/qaguru/qa_guru_3/SelenideTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class SelenideTest {
@@ -22,8 +21,6 @@ class SelenideTest {
         $$(".Box--condensed li").filterBy(exactText("SoftAssertions"))
                 .shouldHaveSize(1);
         $(By.linkText("SoftAssertions")).click();
-        open("/wiki/SoftAssertions");
-        $$("div>pre").filterBy(text("@ExtendWith"))
-                .shouldHaveSize(1);
+        $("[start = '3']").shouldHave(text("Using JUnit5 extend test class:"));
     }
 }
